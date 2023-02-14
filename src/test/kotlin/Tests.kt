7fece75b6ff5521642888d0d16540ec5843c1d6b
LYMorozova <l_morozova@eureca.ru>
2023-02-14 14:18:55 +0300

    Tests implemented

---
+++ src/test/kotlin/Tests.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+internal class SimpleTests {
+    @BeforeEach
+    fun simpleBeforeEachTest() {
+        val randomNum = 7 + 4.2
+        assertTrue(randomNum == 11.2)
+    }
+
+    @Test
+    fun anyTest() {
+        val str = "This is the first test for example"
+        val stringList = str.split(" ")
+        var counter = 0
+        stringList.forEach { word ->
+            counter += word.length
+        }
+        val sum = counter + stringList.size - 1
+        val actual = str.length
+        assertEquals(sum, actual, "It is wrong")
+    }
+}
+
+// Test for TDD
+internal class MusicBrokerTests {
+    @Test
+    fun deleteDiscussionTest() {
+        val discussionId = "345"
+        val discussions = mapOf<String, Discussion>(discussionId to Discussion())
+        deleteDiscussion(discussionId)
+        assertFalse(discussions.keys.contains(discussionId))
+    }
+}

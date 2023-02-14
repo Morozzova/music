8419234bea64eac979df735bc6bc62b500e523ab
LYMorozova <l_morozova@eureca.ru>
2023-02-14 12:07:27 +0300

    Junit dependencies implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
     application
 }
 
@@ -12,8 +12,14 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion: String by project
+val coroutinesVersion: String by project
+val jUnitJupiterVersion: String by project
+
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("test-junit5"))
+    implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    implementation("org.junit.jupiter:junit-jupiter-params:$jUnitJupiterVersion")
 }
 
 tasks.test {

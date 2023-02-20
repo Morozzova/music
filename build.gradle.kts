ef717198e2572ac68393cca93f191018179c65a5
LYMorozova <l_morozova@eureca.ru>
2023-02-20 11:34:07 +0300

    Specs fixed. Configs fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,3 +36,20 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}

15986f7e6ca5805dd97b94fbbd9fc2309570bed4
LYMorozova <l_morozova@eureca.ru>
2023-02-20 19:14:42 +0300

    Config files fixed for jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
-//    application
 }
 
 group = "ru.music"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
 
 val kotestVersion: String by project
 val coroutinesVersion: String by project
@@ -24,17 +19,13 @@ dependencies {
     implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     implementation("org.junit.jupiter:junit-jupiter-params:$jUnitJupiterVersion")
 }
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-//application {
-//    mainClass.set("MainKt")
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = "1.8"
 //}
 
 allprojects {
--- models/build.gradle.kts
+++ models/build.gradle.kts
@@ -1,42 +1,19 @@
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
     id("org.openapi.generator")
-    kotlin("plugin.serialization")
 }
 
-kotlin {
-    jvm { }
-    linuxX64 { }
-    macosX64 { }
+dependencies {
+    val jacksonVersion: String by project
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    testImplementation(kotlin("test-junit"))
+}
 
-    sourceSets {
-        val serializationVersion: String by project
-
-        @Suppress("UNUSED_VARIABLE")
-        val commonMain by getting {
-
-            kotlin.srcDirs("$buildDir/generate-resources/main/src/commonMain/kotlin")
-
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
-            }
-        }
-        @Suppress("UNUSED_VARIABLE")
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        @Suppress("UNUSED_VARIABLE")
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
+sourceSets {
+    main {
+        java.srcDir("$buildDir/generate-resources/main/src/main/kotlin")
     }
 }
 
@@ -44,17 +21,17 @@ kotlin {
  * Настраиваем генерацию здесь
  */
 openApiGenerate {
-    val openapiGroup = "${rootProject.group}.api.v2"
+    val openapiGroup = "${rootProject.group}.api.v1"
     generatorName.set("kotlin") // Это и есть активный генератор
     packageName.set(openapiGroup)
     apiPackage.set("$openapiGroup.api")
     modelPackage.set("$openapiGroup.models")
     invokerPackage.set("$openapiGroup.invoker")
-    inputSpec.set("$rootDir/specs/specs-ad-v2.yaml")
-    library.set("multiplatform")
+    inputSpec.set("$rootDir/specs/specs.yaml")
 
     /**
      * Здесь указываем, что нам нужны только модели, все остальное не нужно
+     * https://openapi-generator.tech/docs/globals
      */
     globalProperties.apply {
         put("models", "")
@@ -69,14 +46,14 @@ openApiGenerate {
         mapOf(
             "dateLibrary" to "string",
             "enumPropertyNaming" to "UPPERCASE",
-            "collectionType" to "list",
+            "serializationLibrary" to "jackson",
+            "collectionType" to "list"
         )
     )
 }
 
-afterEvaluate {
-    val openApiGenerate = tasks.getByName("openApiGenerate")
-    tasks.filter { it.name.startsWith("compile") }.forEach {
-        it.dependsOn(openApiGenerate)
+tasks {
+    compileKotlin {
+        dependsOn(openApiGenerate)
     }
 }

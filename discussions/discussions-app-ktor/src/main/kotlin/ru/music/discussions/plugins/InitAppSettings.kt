77f803cf38b768341ea19c89e8fb57ced60a6099
Piligrim <lubasha-s@yandex.ru>
2023-05-28 20:00:06 +0300

    Auth files, logic and dependencies added to ktor module

--- discussions/discussions-app-ktor/src/main/kotlin/ru/music/discussions/plugins/InitAppSettings.kt
+++ discussions/discussions-app-ktor/src/main/kotlin/ru/music/discussions/plugins/InitAppSettings.kt
@@ -5,6 +5,7 @@ import ru.music.common.DiscCorSettings
 import ru.music.discussions.DiscAppSettings
 import ru.music.discussions.biz.DiscussionsProcessor
 import ru.music.discussions.repository.inmemory.DiscussionsRepoStub
+import ru.music.discussions.ru.music.discussions.base.KtorAuthConfig
 import ru.music.discussions.ru.music.discussions.plugins.DiscussionDbType
 import ru.music.discussions.ru.music.discussions.plugins.getDatabaseConf
 
@@ -20,6 +21,15 @@ fun Application.initAppSettings(): DiscAppSettings {
     return DiscAppSettings(
         appUrls = environment.config.propertyOrNull("ktor.urls")?.getList() ?: emptyList(),
         corSettings = corSettings,
-        processor = DiscussionsProcessor(corSettings)
+        processor = DiscussionsProcessor(corSettings),
+        auth = initAppAuth()
     )
 }
+private fun Application.initAppAuth(): KtorAuthConfig = KtorAuthConfig(
+    secret = environment.config.propertyOrNull("jwt.secret")?.getString() ?: "",
+    issuer = environment.config.property("jwt.issuer").getString(),
+    audience = environment.config.property("jwt.audience").getString(),
+    realm = environment.config.property("jwt.realm").getString(),
+    clientId = environment.config.property("jwt.clientId").getString(),
+    certUrl = environment.config.propertyOrNull("jwt.certUrl")?.getString(),
+)

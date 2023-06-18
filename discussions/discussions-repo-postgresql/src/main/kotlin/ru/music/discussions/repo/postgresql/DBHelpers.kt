a726bc0c7018b4da43927a985b90d95355359632
Piligrim <piligrim@Ubuntu.myguest.virtualbox.org>
2023-06-18 21:04:54 +0300

    Bug with converting DiscAnswer to DB fixed

--- discussions/discussions-repo-postgresql/src/main/kotlin/ru/music/discussions/repo/postgresql/DBHelpers.kt
+++ discussions/discussions-repo-postgresql/src/main/kotlin/ru/music/discussions/repo/postgresql/DBHelpers.kt
@@ -3,9 +3,10 @@ package ru.music.discussions.repo.postgresql
 import ru.music.common.models.DiscAnswer
 
 
-/** Здесь, по-хорошему, нужно делать отдельный CRUD по добавлению ответов в обсуждение, но, в рамках данной работы я реализую только один CRUD. Для MVP такого решения будет достаточно. */
+/** Здесь, по-хорошему, нужно делать отдельный CRUD по добавлению ответов в обсуждение,
+ * но, в рамках данной работы я реализую только один CRUD. Для MVP такого решения будет достаточно. */
 
 fun String.toDBAnswers(): MutableList<DiscAnswer> =
     this.trim().splitToSequence('~').filter { it.isNotEmpty() }.map { DiscAnswer(it) }.toMutableList()
 
-fun MutableList<DiscAnswer>.toDBString(): String = this.joinToString(separator = "~")
+fun MutableList<DiscAnswer>.toDBString(): String = this.joinToString(separator = "~") { it.asString() }
